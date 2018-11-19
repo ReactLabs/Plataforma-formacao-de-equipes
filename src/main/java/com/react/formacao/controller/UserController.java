814e4d65b56dbd4826286fecf4a98aac4c002ba4
brennomatheus <brenno_silva_@hotmail.com>
2018-11-19 20:50:48 -0200

    ajustes layout

---
+++ src/main/java/com/react/formacao/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.react.formacao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/users")
+    @ResponseBody
+    public String getUsers() {
+        return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+                "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+    }
+
+}
